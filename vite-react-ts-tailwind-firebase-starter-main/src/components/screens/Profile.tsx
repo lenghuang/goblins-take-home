a9c2fe4fcda3f7771ad142f14ee43c97c172a6e2
Len Huang <lendevelops@gmail.com>
2024-12-30 17:12:52 -0500

    tweak label button

--- vite-react-ts-tailwind-firebase-starter-main/src/components/screens/Profile.tsx
+++ vite-react-ts-tailwind-firebase-starter-main/src/components/screens/Profile.tsx
@@ -5,18 +5,22 @@ import { useAuthState } from '../contexts/UserContext';
 import { CreateAccountForm } from '../domain/auth/CreateAccountForm';
 import { SignInForm } from '../domain/auth/SignInForm';
 import { SignOutButton } from '../domain/auth/SignOutButton';
+import { StartLabellingButton } from '../domain/label/StartLabellingButton';
 
 function SignedInProfile({ user }: { user: User }) {
   return (
     <>
       <h1 className="text-3xl font-bold mb-2">Profile</h1>
       <p> Welcome back! Here are your details</p>
-      <hr className="m-4" />
+      <hr className="m-8" />
       <p> Email: {user.email} </p>
       <p> User Since: {user.metadata.creationTime} </p>
       <p> Last Sign In: {user.metadata.lastSignInTime} </p>
-      <hr className="m-4" />
-      <SignOutButton />
+      <hr className="m-8" />
+      <div className="flex flex-col gap-4">
+        <StartLabellingButton />
+        <SignOutButton />
+      </div>
     </>
   );
 }
