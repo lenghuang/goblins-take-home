f873821c513832cc6c6deb2229e6c3cf68c0cad7
Len Huang <lendevelops@gmail.com>
2024-12-30 19:35:29 -0500

    mvp for reading from a database

--- vite-react-ts-tailwind-firebase-starter-main/src/components/domain/label/LabelForId.tsx
+++ vite-react-ts-tailwind-firebase-starter-main/src/components/domain/label/LabelForId.tsx
@@ -1,3 +1,33 @@
+import { useGetDocData } from '~/lib/firestore';
+
+interface LabelData {
+  imageUrl: string;
+  labelState: 0 | 1 | 2; // Unlabelled, Partially Labelled, Labelled
+}
+
+const useLabelData = (labelId: string): { data: LabelData | undefined; isLoading: boolean; isError: boolean } => {
+  const { data, isLoading, isError } = useGetDocData('jobs', labelId);
+  if (data?.imageUrl && data?.labelState <= 2 && data?.labelState >= 0) {
+    return { data: data as LabelData, isLoading, isError };
+  }
+
+  return { data: undefined, isLoading: false, isError: true };
+};
+
 export function LabelForId({ labelId }: { labelId: string }) {
-  return <p>labelId: {labelId} </p>;
+  const { data, isLoading, isError } = useLabelData(labelId);
+
+  if (isLoading) {
+    return <p> Loading... </p>;
+  }
+
+  if (isError || !data) {
+    return <p className="text-red-500"> Something went wrong. </p>;
+  }
+
+  return (
+    <div className="p-8 max-w-300">
+      <img className="shadow-lg border-2" src={data.imageUrl}></img>
+    </div>
+  );
 }
--- vite-react-ts-tailwind-firebase-starter-main/src/components/domain/label/MissingLabelId.tsx
+++ vite-react-ts-tailwind-firebase-starter-main/src/components/domain/label/MissingLabelId.tsx
@@ -1,3 +1,3 @@
 export function MissingLabelId() {
-  return <p>Get started with a random label ID</p>;
+  return <p>Get started with a random label ID. Use this for now: wb_b_wb_cmvu1n27qp1fuqq15nk9h</p>;
 }
