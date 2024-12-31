9f94b17fd28c1317f16a4829b218afee45d2e845
Len Huang <lendevelops@gmail.com>
2024-12-31 12:05:49 -0500

    confirm modal with all the data

---
+++ vite-react-ts-tailwind-firebase-starter-main/src/components/domain/label/components/form-utils.ts
@@ -0,0 +1,40 @@
+import { GetSelectedCropsResult } from '~/types/chunkData';
+
+export interface LabelFormInputs {
+  selectedCrops: Array<GetSelectedCropsResult>;
+  inputs: Record<string, string>;
+  confidences: Record<string, number>;
+}
+
+export const setErrorsGivenInputAndConfidence = (
+  src: LabelFormInputs,
+  setErrors: (update: (prev: Record<string, string>) => Record<string, string>) => void,
+) => {
+  const inputKeys = Object.keys(src.inputs);
+  if (src.selectedCrops.length !== inputKeys.length) {
+    const difference = src.selectedCrops.filter((x) => !inputKeys.includes(x.cropId));
+    difference.forEach((x) => setErrors((prev) => ({ ...prev, [x.cropId]: 'Missing math input' })));
+    return;
+  }
+
+  const confidenceKeys = Object.keys(src.confidences);
+  if (src.selectedCrops.length !== confidenceKeys.length) {
+    const difference = src.selectedCrops.filter((x) => !confidenceKeys.includes(x.cropId));
+    difference.forEach((x) => setErrors((prev) => ({ ...prev, [x.cropId]: 'Missing confidence rating' })));
+    return;
+  }
+};
+
+export interface CombinedFormData extends GetSelectedCropsResult {
+  parsedInput: string;
+  parsedInputConfidence: number;
+}
+
+export const getCombinedLabelFormData = (src: LabelFormInputs): Record<string, CombinedFormData> => {
+  return Object.fromEntries(
+    src.selectedCrops.map((crop) => [
+      crop.cropId,
+      { ...crop, parsedInput: src.inputs[crop.cropId], parsedInputConfidence: src.confidences[crop.cropId] },
+    ]),
+  );
+};
