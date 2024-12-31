a4b09388ef8d2a6f5fe452c4aafa04d0149d7485
Len Huang <lendevelops@gmail.com>
2024-12-31 11:14:32 -0500

    get a submit button going

--- vite-react-ts-tailwind-firebase-starter-main/src/components/domain/label/LabelForId.tsx
+++ vite-react-ts-tailwind-firebase-starter-main/src/components/domain/label/LabelForId.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useSelectedCropsData } from '../crop/CropPreview/utilities/useSelectedCropsData';
 import { LabelCard } from './LabelCard';
 import '//unpkg.com/mathlive';
@@ -8,6 +8,8 @@ export function LabelForId({ labelId }: { labelId: string }) {
   const [inputs, setInputs] = useState<Record<string, string>>({});
   const [blurredInputs, setBlurredInputs] = useState<Record<string, string>>({});
   const [confidences, setConfidences] = useState<Record<string, number>>({});
+  const [errors, setErrors] = useState<Record<string, string>>({});
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   if (isSelectedCropsLoading) {
     return <p> Loading... </p>;
@@ -16,6 +18,35 @@ export function LabelForId({ labelId }: { labelId: string }) {
   if (isSelectedCropsError || !selectedCrops) {
     return <p className="text-red-500"> Something went wrong. </p>;
   }
+
+  const onClick = () => {
+    setErrors({});
+
+    const inputKeys = Object.keys(inputs);
+    if (selectedCrops.length !== inputKeys.length) {
+      const difference = selectedCrops.filter((x) => !inputKeys.includes(x.cropId));
+      difference.forEach((x) => setErrors((prev) => ({ ...prev, [x.cropId]: 'Missing math input' })));
+      return;
+    }
+
+    const confidenceKeys = Object.keys(confidences);
+    if (selectedCrops.length !== confidenceKeys.length) {
+      const difference = selectedCrops.filter((x) => !confidenceKeys.includes(x.cropId));
+      difference.forEach((x) => setErrors((prev) => ({ ...prev, [x.cropId]: 'Missing confidence rating' })));
+      return;
+    }
+
+    const combinedData = Object.fromEntries(
+      selectedCrops.map((crop) => [
+        crop.cropId,
+        { ...crop, parsedInput: inputs[crop.cropId], parsedInputConfidence: confidences[crop.cropId] },
+      ]),
+    );
+
+    setIsModalOpen(true);
+
+    console.log(combinedData);
+  };
 
   return (
     <>
@@ -35,11 +66,48 @@ export function LabelForId({ labelId }: { labelId: string }) {
                 blurredInputValue={blurredInputs[crop.cropId]}
                 handleBlurredInputs={(value: string) => setBlurredInputs((prev) => ({ ...prev, [crop.cropId]: value }))}
                 onConfidenceChange={(value: number) => setConfidences((prev) => ({ ...prev, [crop.cropId]: value }))}
+                error={errors[crop.cropId]}
               />
             </div>
           ))}
+          <button onClick={onClick} className="btn btn-primary w-full">
+            Submit Your Labels
+          </button>
+          <ConfirmLabelModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
         </div>
       </div>
     </>
   );
 }
+
+const ConfirmLabelModal = ({
+  isModalOpen,
+  setIsModalOpen,
+}: {
+  isModalOpen: boolean;
+  setIsModalOpen: (arg: boolean) => void;
+}) => {
+  const modalRef = useRef<HTMLDialogElement | null>(null);
+
+  useEffect(() => {
+    if (isModalOpen && modalRef.current) {
+      modalRef.current.showModal();
+    } else if (!isModalOpen && modalRef.current) {
+      modalRef.current.close();
+    }
+  }, [isModalOpen]);
+
+  return (
+    <dialog ref={modalRef} className="modal">
+      <div className="modal-box w-11/12 max-w-5xl">
+        <h3 className="font-bold text-lg">Hello!</h3>
+        <p className="py-4">Click the button below to close</p>
+        <div className="modal-action">
+          <button onClick={() => setIsModalOpen(false)} className="btn">
+            Close
+          </button>
+        </div>
+      </div>
+    </dialog>
+  );
+};
