b8af5b04b56b499d28f4f9f27f795b398ab63fec
Len Huang <lendevelops@gmail.com>
2024-12-31 14:54:27 -0500

    tweak label button to not set state on render

--- vite-react-ts-tailwind-firebase-starter-main/src/components/domain/label/components/SubmitLabelsButton.tsx
+++ vite-react-ts-tailwind-firebase-starter-main/src/components/domain/label/components/SubmitLabelsButton.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useUpsertDocs } from '~/lib/firestore';
 import { CombinedFormData } from './form-utils';
@@ -18,35 +18,36 @@ export const SubmitLabelsButton = ({
 
   const isLoading = isLoadingInternal || updateChunksMutation.isPending;
 
-  if (updateChunksMutation.isSuccess) {
-    clearStateCallback();
-    navigate(`/crop?congratsCount=${Object.keys(combinedData).length}`); // Show a good job banner when they finish!
-  }
+  useEffect(() => {
+    // Check for successful mutation, then clear state and navigate
+    if (!isLoading && updateChunksMutation.isSuccess) {
+      clearStateCallback();
+      navigate(`/crop?congratsCount=${Object.keys(combinedData).length}`); // Show a good job banner when they finish!
+    }
+  }, [updateChunksMutation.isSuccess, combinedData, clearStateCallback, navigate]);
+
+  const handleSubmit = () => {
+    setIsLoading(true);
+    // Rename "cropId" to "documentId"
+    const newData = Object.values(combinedData).map((x: CombinedFormData) => ({
+      documentId: x.cropId,
+      croppedImageSrc: x.croppedImageSrc,
+      parsedInput: x.parsedInput,
+      parsedInputConfidence: x.parsedInputConfidence,
+      uploadDate: x.uploadDate,
+      uploadedBy: x.uploadedBy,
+    }));
+
+    updateChunksMutation.mutate(newData);
+    setIsLoading(false);
+  };
 
   return (
     <>
       <button disabled={isLoading} onClick={editLabelsCallback} className="btn">
         Make More Changes
       </button>
-      <button
-        disabled={isLoading}
-        onClick={() => {
-          setIsLoading(true);
-          // Rename "cropId" to "documentId"
-          const newData = Object.values(combinedData).map((x: CombinedFormData) => ({
-            documentId: x.cropId,
-            croppedImageSrc: x.croppedImageSrc,
-            parsedInput: x.parsedInput,
-            parsedInputConfidence: x.parsedInputConfidence,
-            uploadDate: x.uploadDate,
-            uploadedBy: x.uploadedBy,
-          }));
-
-          updateChunksMutation.mutate(newData);
-          setIsLoading(false);
-        }}
-        className={`btn btn-primary ${isLoading ? 'loading' : ''}`}
-      >
+      <button disabled={isLoading} onClick={handleSubmit} className={`btn btn-primary ${isLoading ? 'loading' : ''}`}>
         {isLoading ? 'Loading...' : 'Submit Labels'}
       </button>
     </>
