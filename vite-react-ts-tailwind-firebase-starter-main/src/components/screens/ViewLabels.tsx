8d7a4e66e6ec10a979a2af77de929fab6baa2e2d
Len Huang <lendevelops@gmail.com>
2024-12-31 18:00:58 -0500

    broken pagination but graph is showing

--- vite-react-ts-tailwind-firebase-starter-main/src/components/screens/ViewLabels.tsx
+++ vite-react-ts-tailwind-firebase-starter-main/src/components/screens/ViewLabels.tsx
@@ -2,37 +2,31 @@ import { useState } from 'react';
 import { formatFirestoreTimestampAgo } from '~/lib/dates';
 import { useGetPaginatedDocs } from '~/lib/firestore';
 
+const PAGE_SIZE = 3;
+
 function usePaginatedLabelledCrops() {
-  const [pageNumber, setPageNumber] = useState(0);
-  const [lastDocs, setLastDocs] = useState<any[]>([]); // Stack of lastDocs
+  const [pageSize, setPageSize] = useState(PAGE_SIZE);
+  const [lastDocs, setLastDocs] = useState<any[]>([]);
 
   // I was unable to figure out composite indexes and querying quickly enough, so I am
   // just doing it in memory for now, which is a bad practice typically.
   const { data, isLoading, isError } = useGetPaginatedDocs(
     'chunks',
     'documentId',
-    pageNumber,
-    20,
+    1,
+    pageSize,
     lastDocs[lastDocs.length - 1],
     // orderBy('parsedInput'),
     // where('parsedInput', '!=', ''),
   );
 
   const labelledCropsData = data?.documents || [];
-  const isLastPage = labelledCropsData.length < 6 || !data?.lastVisible;
+  const isLastPage = labelledCropsData.length < PAGE_SIZE || !data?.lastVisible;
 
-  // Go to the previous page
-  const prevPage = () => {
-    if (lastDocs.length === 0) return;
-    setLastDocs(lastDocs.slice(0, -1));
-    setPageNumber((prev) => prev - 1);
-  };
-
-  // Go to the next page
   const nextPage = () => {
     if (data?.lastVisible) {
       setLastDocs((prev) => [...prev, data.lastVisible]);
-      setPageNumber((prev) => prev + 1);
+      setPageSize((prev) => (prev += PAGE_SIZE));
     }
   };
 
@@ -40,51 +34,78 @@ function usePaginatedLabelledCrops() {
     labelledCropsData,
     isLabelledCropsLoading: isLoading,
     isLabelledCropsError: isError,
-    prevPage,
     nextPage,
-    pageNumber,
     isLastPage,
   };
 }
 
 export default function LabelledCropsPage() {
-  const {
-    labelledCropsData,
-    isLabelledCropsLoading,
-    isLabelledCropsError,
-    prevPage,
-    nextPage,
-    pageNumber,
-    isLastPage,
-  } = usePaginatedLabelledCrops();
+  const { labelledCropsData, isLabelledCropsLoading, isLabelledCropsError, nextPage, isLastPage } =
+    usePaginatedLabelledCrops();
 
   if (isLabelledCropsLoading) return <div>Loading...</div>;
   if (isLabelledCropsError) return <div>Error loading labelled crops.</div>;
 
   return (
     <div>
-      <div className="mt-8 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {labelledCropsData?.map((doc) => (
-          <div key={doc.documentId} className="border card card-compact w-full bg-base-100 shadow-xl">
-            <figure className="border-b">
-              <img src={doc.croppedImageSrc} className="object-contain w-full h-48" />
-            </figure>
-            <div className="card-body card-action">
-              <p>{doc.uploadedBy}</p>
-              <p>{formatFirestoreTimestampAgo(doc.uploadDate)}</p>
-              <p>{doc.parsedInput}</p>
-              <p>{doc.parsedInputConfidence}</p>
-            </div>
-          </div>
-        ))}
+      <h2 className="text-2xl font-bold mb-6 text-center">Labelled Crops</h2>
+      <div className="overflow-x-auto">
+        <table className="table table-fixed min-w-full">
+          <thead>
+            <tr>
+              <th className="w-1/12">#</th>
+              <th className="w-1/12">Document Id</th>
+              <th className="w-1/12">ImageURL</th>
+              <th className="w-1/12">Image</th>
+              <th className="w-4/12">Math Input</th>
+              <th className="w-1/12">Confidence</th>
+              <th className="w-2/12">Uploaded By</th>
+              <th className="w-1/12">Upload Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.values(labelledCropsData).map((label, index) => {
+              const { documentId, croppedImageSrc, parsedInput, parsedInputConfidence, uploadedBy, uploadDate } = label;
+              return (
+                <tr key={`table_row_${documentId}`}>
+                  <th>{index + 1}</th>
+                  <td>
+                    <div className="break-all line-clamp-3 overflow-hidden text-ellipsis">{documentId}</div>
+                  </td>
+                  <td>
+                    <div className="break-all line-clamp-3 overflow-hidden text-ellipsis">{croppedImageSrc}</div>
+                  </td>
+                  <td>
+                    <div className="break-all line-clamp-3 overflow-hidden text-ellipsis">
+                      <img className="contain" src={croppedImageSrc} />
+                    </div>
+                  </td>
+                  <td>
+                    <div className="break-all line-clamp-3 overflow-hidden text-ellipsis">
+                      <code>{parsedInput}</code>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="break-all line-clamp-3 overflow-hidden text-ellipsis">{parsedInputConfidence}</div>
+                  </td>
+                  <td>
+                    <div className="break-all line-clamp-3 overflow-hidden text-ellipsis">{uploadedBy}</div>
+                  </td>
+                  <td>
+                    <div className="break-all line-clamp-3 overflow-hidden text-ellipsis">
+                      {formatFirestoreTimestampAgo(uploadDate)}
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
 
-      <div className="mt-8 flex gap-4 justify-center">
-        <button onClick={prevPage} className="btn" disabled={pageNumber === 0}>
-          Previous
-        </button>
+      <div className="flex justify-center gap-4 mt-8">
         <button onClick={nextPage} className="btn btn-primary" disabled={isLastPage}>
-          Next
+          Load More
         </button>
       </div>
     </div>
