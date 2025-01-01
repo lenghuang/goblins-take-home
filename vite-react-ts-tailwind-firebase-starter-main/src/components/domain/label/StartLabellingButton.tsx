65b69ade33442cab589ba5796f5560127304f805
Len Huang <lendevelops@gmail.com>
2025-01-01 17:52:20 -0500

    renaming files

--- vite-react-ts-tailwind-firebase-starter-main/src/components/domain/label/StartLabellingButton.tsx
+++
@@ -1,7 +0,0 @@
-export function StartLabellingButton() {
-  return (
-    <a href="/crop">
-      <button className="btn btn-primary w-full text-white">Start Labelling</button>
-    </a>
-  );
-}
--- vite-react-ts-tailwind-firebase-starter-main/src/components/shared/Container.tsx
+++
@@ -1,5 +0,0 @@
-export const Container = ({ children }: { children: any }) => (
-  <div className="flex flex-col items-center w-full min-h-full">
-    <div className="p-8 w-full max-w-320">{children}</div>
-  </div>
-);
